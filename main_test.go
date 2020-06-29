ff5c83c18fcd38fe9aad08e9b1d60ca9e3783cab
Rid-lin <vlad@vegner.org>
2020-06-30 01:33:08 +0500

    add test for 1 function

--- main_test.go
+++ main_test.go
@@ -1,68 +1,41 @@
 package main
 
-// flow-cat /home/rid-lin/flow/gw_root_75km/2020/2020-06/2020-06-23/ft-v05.2020-06-23.095336+0500 | flow-print -f 5
-// Start             End               Sif   SrcIPaddress    SrcP  DIf   DstIPaddress    DstP    P Fl Pkts
+import "testing"
 
-var testingInData = `
-0623.09:53:19.589 0623.09:53:20.649 8     192.168.65.155  50182 7     45.142.212.204  3128  6   0  3          144
-0623.09:53:20.669 0623.09:53:20.669 8     192.168.65.195  55245 9     10.61.152.10    445   6   0  1          41
-0623.09:53:20.669 0623.09:53:20.669 9     10.61.152.10    445   8     192.168.65.195  55245 6   0  1          52
-0623.09:53:20.749 0623.09:53:20.749 8     192.168.65.143  38786 13    87.250.251.119  443   6   1  1          52
-0623.09:52:34.249 0623.09:53:20.879 13    217.20.152.247  443   8     192.168.65.82   40192 6   2  47         41724`
+func TestCheckIP(t *testing.T) {
+	testCases := []struct {
+		name    string
+		ip      string
+		subnet  string
+		isValid bool
+	}{
+		{
+			name:    "valid",
+			ip:      "192.168.0.1",
+			subnet:  "192.168.0.0/24",
+			isValid: true,
+		},
+		{
+			name:    "Invalid IP",
+			ip:      "192.168.1.1",
+			subnet:  "192.168.0.0/24",
+			isValid: false,
+		},
+		{
+			name:    "Invalid Mask",
+			ip:      "192.168.0.1",
+			subnet:  "192.168.0.0/8",
+			isValid: false,
+		},
+	}
+	for _, tc := range testCases {
+		result, err := checkIP(tc.subnet, tc.ip)
+		if err != nil {
+			t.Errorf("Test 'сheckIP' failed. Error:%v", err)
+		}
+		if result != tc.isValid {
+			t.Errorf("Test 'сheckIP' OK")
+		}
+	}
 
-var testingInLine = "0623.09:52:34.249 0623.09:53:20.879 13    217.20.152.247  443   8     192.168.65.82   40192 6   2  47         41724"
-var testingOutLineOK = ""
-var testingunixStampFromNetflowDateStr = "0623.09:52:34.249"
-var testingunixStampFromNetflowDateStrOK = "1592905954.249"
-var testingunixStampFromNetflowDate = "0623.09:52:34.249"
-var testingunixStampFromNetflowDateOK int64 = 1592905954249
-
-// func TestParseNetFlowToSquidLine(t *testing.T) {
-// 	result, err := parseNetFlowToSquidLine(testingInLine, "2020", "192.168.65.1")
-// 	if err != nil {
-// 		t.Errorf("Test OK failed: %s", err)
-// 	}
-// 	if result == testingOutLineOK {
-// 		t.Errorf("Test OK failed, result not match")
-// 	}
-// }
-
-// func TestUnixStampFromNetflowDateStr(t *testing.T) {
-// 	result := unixStampFromNetflowDateStr(testingunixStampFromNetflowDateStr, "2020")
-// 	if result != testingunixStampFromNetflowDateStrOK {
-// 		t.Errorf("Test 'unixStampFromNetflowDate' failed, result not match")
-// 	}
-// 	result = unixStampFromNetflowDateStr(testingunixStampFromNetflowDateStr, "2019")
-// 	if result == testingunixStampFromNetflowDateStrOK {
-// 		t.Errorf("Test 'unixStampFromNetflowDate' with wrong data failed,  result not match")
-// 	}
-// 	result = unixStampFromNetflowDateStr(testingunixStampFromNetflowDateStr, "20")
-// 	if result != "" {
-// 		t.Errorf("Test 'unixStampFromNetflowDate' with wrong data failed, return wrong result")
-// 	}
-// }
-
-// func TestUnixStampFromNetflowDate(t *testing.T) {
-// 	result := unixStampFromNetflowDate(testingunixStampFromNetflowDate, "2020")
-// 	if result != testingunixStampFromNetflowDateOK {
-// 		t.Errorf("Test 'unixStampFromNetflowDate' failed, result not match")
-// 	}
-// 	result = unixStampFromNetflowDate(testingunixStampFromNetflowDate, "2019")
-// 	if result == testingunixStampFromNetflowDateOK {
-// 		t.Errorf("Test 'unixStampFromNetflowDate' with wrong data failed,  result not match")
-// 	}
-// 	result = unixStampFromNetflowDate(testingunixStampFromNetflowDate, "20")
-// 	if result != 0 {
-// 		t.Errorf("Test 'unixStampFromNetflowDate' with wrong data failed, return wrong result")
-// 	}
-// }
-
-// func TestFail(t *testing.T) {
-// 	result, err := parseNetFlowToSquidLine(testingInLine)
-// 	if err != nil {
-// 		t.Errorf("Test OK failed: %s", err)
-// 	}
-// 	if result != testingOutLineOK {
-// 		t.Errorf("Test OK failed, result not match")
-// 	}
-// }
+}
